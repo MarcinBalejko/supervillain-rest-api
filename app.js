00c3be18e89c3749317f2470b85d46461b4a7bd2
MarcinBalejko <mbbm9@o2.pl>
2019-12-25 23:17:30 +0100

    :white_check_mark: Database connection + dotenv

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
-
+dotenv.config();
 
 // Connect to DB
-mongoose.connect()
+mongoose.connect(
+    process.env.DB_CONNECT, { useUnifiedTopology: true }, () =>
+    console.log('connected to db!')
+);
+
 // ROUTES
 app.get('/', (req, res) => {
     res.send('We are on home');
