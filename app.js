cf96126dc786ead87e2ba266e25d739d21ff76a2
MarcinBalejko <mbbm9@o2.pl>
2019-12-25 21:43:01 +0100

    :white_check_mark: Added villains list route + mongoose

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
 
+
+
+// Connect to DB
+mongoose.connect()
 // ROUTES
 app.get('/', (req, res) => {
     res.send('We are on home');
 });
 
-app.listen(3000);
+app.get('/villains', (req, res) => {
+    res.send('We are on the villains list');
+});
+
+app.listen(3000, () => console.log('Server Up and running'));
