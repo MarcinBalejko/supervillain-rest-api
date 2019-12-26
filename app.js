00d31ed3328471fd3f98b02344e7be3b9e249eb6
MarcinBalejko <mbbm9@o2.pl>
2019-12-26 02:19:01 +0100

    :white_check_mark: Added body-parser

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv/config');
+
+app.use(bodyParser.json());   // Anytime we hit any request the body-parser will run
 
 
 // Import Routes
--- routes/villains.js
+++ routes/villains.js
@@ -1,17 +1,22 @@
 const express = require('express');
-
 const router = express.Router();
+const Villain = require('../models/Villain');
 
 
 
 // ROUTES
 
 router.get('/', (req, res) => {
-    res.send('We are on the villains list');
+    res.send('We are on the villains');
 });
 
 router.get('/specific', (req, res) => {
     res.send('Specific villain')
 });
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+});
+
 
 module.exports = router;
